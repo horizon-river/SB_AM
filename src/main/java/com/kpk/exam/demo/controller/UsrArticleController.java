fd89f7f0b21cfa795bd24e2d06a5b16969cc6e39
horizon-river <kimpk0416@gmail.com>
2022-09-26 16:50:46 +0900

    게시물 테스트데이터 10개 생성

--- src/main/java/com/kpk/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kpk/exam/demo/controller/UsrArticleController.java
@@ -17,8 +17,23 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		this.articles = new ArrayList<>();
 		this.lastArticleId = 0;
+		
+		makeTestData();
 	}
 	
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			int id = lastArticleId + 1;
+			String title = "제목" + id;
+			String body = "내용" + id;
+			
+			Article article = new Article(id, title, body);
+			
+			articles.add(article);
+			lastArticleId = id;
+		}
+	}
+
 	@RequestMapping("usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
