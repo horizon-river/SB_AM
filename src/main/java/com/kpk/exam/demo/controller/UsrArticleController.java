a3f1a6af7c36f4e87368f9cc4e33941754297b78
horizon-river <kimpk0416@gmail.com>
2022-09-26 18:15:38 +0900

    Object Class, 게시물 상세보기 기능 구현

--- src/main/java/com/kpk/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kpk/exam/demo/controller/UsrArticleController.java
@@ -87,24 +87,35 @@ public class UsrArticleController {
 	public String doDelete(int id) {
 		Article article = getArticle(id);
 		if (article == null) {
-			return id + "번 글은 존재하지 않습니다.";
+			return id + "번 게시물은 존재하지 않습니다.";
 		}
 		
 		deleteArticle(id);
 		
-		return id + "번 글이 삭제되었습니다.";
+		return id + "번 게시물이 삭제되었습니다.";
 	}
 	
 	@RequestMapping("usr/article/doModify")
 	@ResponseBody
-	public String doModify(int id, String title, String body) {
+	public Object doModify(int id, String title, String body) {
 		Article article = getArticle(id);
 		if (article == null) {
-			return id + "번 글은 존재하지 않습니다.";
+			return id + "번 게시물은 존재하지 않습니다.";
 		}
 		
 		article = modifyArticle(id, title, body);
 		
-		return id + "번 게시물을 수정했습니다.";
+		return article;
+	}
+	
+	@RequestMapping("usr/article/getArticle")
+	@ResponseBody
+	public Object getArticleAction(int id) {
+		Article article = getArticle(id);
+		if (article == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+		
+		return article;
 	}
 }
