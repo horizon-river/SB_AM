95c859e62d25d0cc27534459a8d209961abea529
horizon-river <kimpk0416@gmail.com>
2022-09-26 17:48:28 +0900

    게시물 수정 기능 구현

--- src/main/java/com/kpk/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kpk/exam/demo/controller/UsrArticleController.java
@@ -47,6 +47,15 @@ public class UsrArticleController {
 		
 		articles.remove(article);
 	}
+
+	private Article modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		article.setTitle(title);
+		article.setBody(body);
+		
+		return article;
+	}
 	
 	private Article writeArticle(String title, String body) {
 		int id = lastArticleId + 1;
@@ -84,5 +93,18 @@ public class UsrArticleController {
 		deleteArticle(id);
 		
 		return id + "번 글이 삭제되었습니다.";
+	}
+	
+	@RequestMapping("usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+		if (article == null) {
+			return id + "번 글은 존재하지 않습니다.";
+		}
+		
+		article = modifyArticle(id, title, body);
+		
+		return id + "번 게시물을 수정했습니다.";
 	}
 }
