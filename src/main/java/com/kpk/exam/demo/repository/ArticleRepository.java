a1339e6524856b0d03e78394ca5b6f1f34c31eb9
horizon-river <kimpk0416@gmail.com>
2022-09-27 18:47:22 +0900

    INSERT 쿼리 실행되도록 return 타입 수정, last_insert_id()

--- src/main/java/com/kpk/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/kpk/exam/demo/repository/ArticleRepository.java
@@ -14,7 +14,8 @@ import com.kpk.exam.demo.vo.Article;
 public interface ArticleRepository {
 	
 	// INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = ?, `body` = ?
-	public Article writeArticle(String title, String body);
+	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+	public void writeArticle(String title, String body);
 	
 	// SELECT * FROM article WHERE id = ?;
 	@Select("SELECT * FROM article WHERE id = #{id}")
@@ -31,5 +32,8 @@ public interface ArticleRepository {
 	// UPDATE article SET title = ?, `body` = ?, updateDate = NOW() WHERE id = ?;
 	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
 	public Article modifyArticle(int id, String title, String body);
+
+	@Select("SELECT LAST_INSERT_ID()")
+	public int getLastInsertId();
 	
 }
