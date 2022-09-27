e333e1e29f664f5d4e4d9fc6696af4f395104257
horizon-river <kimpk0416@gmail.com>
2022-09-27 16:39:22 +0900

    ArticleService에 있는 로직을 ArticleRepository로 이전

--- src/main/java/com/kpk/exam/demo/service/ArticleService.java
+++ src/main/java/com/kpk/exam/demo/service/ArticleService.java
@@ -1,71 +1,43 @@
 package com.kpk.exam.demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kpk.exam.demo.repository.ArticleRepository;
 import com.kpk.exam.demo.vo.Article;
 
 @Service
 public class ArticleService {
-	private int lastArticleId;
-	private List<Article> articles;
+	
+	@Autowired
+	private ArticleRepository articleRepository;
 	
 	// 생성자
-	public ArticleService() {		
-		this.articles = new ArrayList<>();
-		this.lastArticleId = 0;
-		
-		makeTestData();
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+		articleRepository.makeTestData();
 	}
 	
-	// 서비스메서드
-		private void makeTestData() {
-			for (int i = 1; i <= 10; i++) {
-				String title = "제목" + i;
-				String body = "내용" + i;
-				
-				writeArticle(title, body);
-			}
-		}
-		
-		public Article getArticle(int id) {
-			for (Article article : articles) {
-				if(article.getId() == id) {
-					return article;
-				}
-			}
-			return null;
-		}
-		
-		public void deleteArticle(int id) {
-			Article article = getArticle(id);
-			
-			articles.remove(article);
-		}
+	// 서비스메서드	
+	public Article getArticle(int id) {
+		return articleRepository.getArticle(id);
+	}
+	
+	public List<Article> getArticles() {
+		return articleRepository.getArticles();
+	}
+	
+	public Article writeArticle(String title, String body) {
+		return articleRepository.writeArticle(title, body);
+	}
+	
+	public void deleteArticle(int id) {
+		articleRepository.deleteArticle(id);
+	}
 
-		public Article modifyArticle(int id, String title, String body) {
-			Article article = getArticle(id);
-			
-			article.setTitle(title);
-			article.setBody(body);
-			
-			return article;
-		}
-		
-		public Article writeArticle(String title, String body) {
-			int id = lastArticleId + 1;
-			Article article = new Article(id, title, body);
-			
-			articles.add(article);
-			lastArticleId = id;
-			
-			return article;
-		}
-
-		public List<Article> articles() {
-			return articles;
-		}
-
+	public Article modifyArticle(int id, String title, String body) {
+		return articleRepository.modifyArticle(id, title, body);
+	}
 }
