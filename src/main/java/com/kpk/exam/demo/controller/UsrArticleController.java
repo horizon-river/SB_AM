4a455993fb652d88a6a8af7ddfd56bb61fdce3ab
horizon-river <kimpk0416@gmail.com>
2022-09-30 16:22:07 +0900

    usr/article/getArticle에 ResultData 적용

--- src/main/java/com/kpk/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kpk/exam/demo/controller/UsrArticleController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kpk.exam.demo.service.ArticleService;
+import com.kpk.exam.demo.util.Ut;
 import com.kpk.exam.demo.vo.Article;
+import com.kpk.exam.demo.vo.ResultData;
 
 @Controller
 public class UsrArticleController {
@@ -61,12 +63,12 @@ public class UsrArticleController {
 	
 	@RequestMapping("usr/article/getArticle")
 	@ResponseBody
-	public Object getArticle(int id) {
+	public ResultData getArticle(int id) {
 		Article article = articleService.getArticle(id);
 		if (article == null) {
-			return id + "번 게시물은 존재하지 않습니다.";
+			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다.", id));
 		}
 		
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), article);
 	}
 }
