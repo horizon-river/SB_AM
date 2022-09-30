5f7fd22e9ab035ed648bad0bf6da1d0c3bea6783
horizon-river <kimpk0416@gmail.com>
2022-09-30 17:46:29 +0900

    usr/member/doJoin에 ResultData 적용

--- src/main/java/com/kpk/exam/demo/service/MemberService.java
+++ src/main/java/com/kpk/exam/demo/service/MemberService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kpk.exam.demo.repository.MemberRepository;
+import com.kpk.exam.demo.util.Ut;
 import com.kpk.exam.demo.vo.Member;
+import com.kpk.exam.demo.vo.ResultData;
 
 @Service
 public class MemberService {
@@ -12,22 +14,23 @@ public class MemberService {
 	@Autowired
 	private MemberRepository memberRepository;
 
-	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+	public ResultData join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
 		// 로그인아이디 중복체크
 		Member existsMember = memberRepository.getMemberByLoginId(loginId);
 		
 		if (existsMember != null) {
-			return -1;
+			return ResultData.from("F-7", Ut.f("이미 사용중인 아이디(%s)입니다.", loginId));
 		}
 		// 이름 + 이메일 중복체크
 		existsMember = getMemberByNameAndEmail(name, email);
 		
 		if(existsMember != null) {
-			return -2;
+			return ResultData.from("F-8", Ut.f("이미 사용중인 이름(%s)과 이메일(%s)입니다.", name, email));
 		}
 		
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNum, email);
-		return memberRepository.getLastInsertId();
+		int id = memberRepository.getLastInsertId();
+		return ResultData.from("S-1", "회원가입이 완료되었습니다.", id);
 	}
 
 	private Member getMemberByNameAndEmail(String name, String email) {
