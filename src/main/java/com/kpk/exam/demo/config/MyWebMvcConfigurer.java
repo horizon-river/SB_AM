a2eb5c218d044d7262d1de14f7ac99ae9ac86259
horizon-river <kimpk0416@gmail.com>
2022-10-12 16:41:59 +0900

    NeedLoginInterceptor 구현 및 MyWebMvcConfigurer의 registry 등록

--- src/main/java/com/kpk/exam/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/kpk/exam/demo/config/MyWebMvcConfigurer.java
@@ -1,4 +1,4 @@
-package com.kpk.exam.demo;
+package com.kpk.exam.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -6,17 +6,27 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.kpk.exam.demo.interceptor.BeforeActionInterceptor;
+import com.kpk.exam.demo.interceptor.NeedLoginInterceptor;
 
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
-	
+	// BeforeActionInterceptor 불러오기
 	@Autowired
 	BeforeActionInterceptor beforeActionInterceptor;
+	
+	// NeedLoginInterceptor 불러오기
+	@Autowired
+	NeedLoginInterceptor needLoginInterceptor;
 	
 	// 인터셉터 적용
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
-				.excludePathPatterns("/error");
+		.excludePathPatterns("/error");
+		
+		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/write")
+		.addPathPatterns("/usr/article/doAdd").addPathPatterns("/usr/article/modify")
+		.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/doDelete")
+		.excludePathPatterns("/resource/**").excludePathPatterns("/error");
 	}
 	
 }
