0379d225f867f6b649cd5d0b3e0ae52d32c6e589
horizon-river <kimpk0416@gmail.com>
2022-10-20 18:38:49 +0900

    조회수 증가 액션의 방식을 수정, 증가한 조회수를 볼 수 있도록

--- src/main/java/com/kpk/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kpk/exam/demo/controller/UsrArticleController.java
@@ -149,17 +149,22 @@ public class UsrArticleController {
 	
 	@RequestMapping("usr/article/detail")
 	public String getArticle(Model model, int id) {
-		
-		ResultData<Integer> increaseHitCountRd = articleService.increaseHitCount(id);
-		
-		if(increaseHitCountRd.isFail()) {
-			return rq.jsHistoryBackOnView(increaseHitCountRd.getMsg());
-		}
-		
 		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 
 		model.addAttribute("article", article);
 		
 		return "usr/article/detail";
+	}
+	
+	@RequestMapping("/usr/article/doIncreaseHitCount")
+	public ResultData<Integer> doIncreaseHitCount(int id) {
+		ResultData<Integer> increaseHitCountRd = articleService.increaseHitCount(id);
+		
+		if(increaseHitCountRd.isFail()) {
+			return increaseHitCountRd;
+		}
+		
+		return ResultData.newData(increaseHitCountRd, "hitCount", articleService.getArticleHitCount(id));
+		
 	}
 }
