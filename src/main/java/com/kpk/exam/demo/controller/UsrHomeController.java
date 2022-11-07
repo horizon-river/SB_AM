a3518ae40917c33b2da43193e4a446826ad25a02
horizon-river <kimpk0416@gmail.com>
2022-11-07 17:22:45 +0900

    처음부터 무조건 rq 객체를 만드는 방식을 수정, 필요할 때만 생성

--- src/main/java/com/kpk/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/kpk/exam/demo/controller/UsrHomeController.java
@@ -2,13 +2,21 @@ package com.kpk.exam.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kpk.exam.demo.vo.Rq;
 
 @Controller
 public class UsrHomeController {
+	private Rq rq;
 	
+	public UsrHomeController(Rq rq) {
+		this.rq = rq;
+	}
+
 	@RequestMapping("/usr/home/main")
 	public String showMain() {
+		rq.runA();
+		
 		return "usr/home/main";
 	}
 	
