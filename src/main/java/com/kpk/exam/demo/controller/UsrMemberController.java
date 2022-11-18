42efea368ece96075766b7d5ae4c2212a750c50c
horizon-river <kimpk0416@gmail.com>
2022-11-18 18:47:57 +0900

    MemberController 측에서 이중으로 체크하는 부분 수정

--- src/main/java/com/kpk/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/kpk/exam/demo/controller/UsrMemberController.java
@@ -29,26 +29,7 @@ public class UsrMemberController {
 	@RequestMapping("usr/member/doJoin")
 	@ResponseBody
 	public String doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email, @RequestParam(defaultValue = "/") String afterLoginUri) {
-		if (Ut.empty(loginId)) {
-			return rq.jsHistoryBack("F-1", "아이디를 입력해주세요.");
-		}
-		if (Ut.empty(loginPw)) {
-			return rq.jsHistoryBack("F-2", "비밀번호를 입력해주세요.");
-		}
-		if (Ut.empty(name)) {
-			return rq.jsHistoryBack("F-3", "이름을 입력해주세요.");
-		}
-		if (Ut.empty(nickname)) {
-			return rq.jsHistoryBack("F-4", "닉네임을 입력해주세요.");
-		}
-		if (Ut.empty(cellphoneNum)) {
-			return rq.jsHistoryBack("F-5", "전화번호를 입력해주세요.");
-		}
-		if (Ut.empty(email)) {
-			return rq.jsHistoryBack("F-6", "이메일을 입력해주세요.");
-		}
-		
-		
+				
 		ResultData<Integer> joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
 		
 		if (joinRd.isFail()) {
